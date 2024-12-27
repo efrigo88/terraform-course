157b008fd7c2b95a706c7f0c54f068b645395d37
Emiliano Frigo <emif.iron@gmail.com>
2024-12-27 19:32:00 +0100

    add sh to delete terraform state related files

---
+++ delete_local_tf_states.sh
@@ -0,0 +1,11 @@
+# Find and delete all .terraform directories
+find . -type d -name ".terraform" -exec rm -rf {} +
+
+# Find and delete specific terraform state files
+find . -type f \( \
+    -name ".terraform.lock.hcl" \
+    -o -name ".terraform.tfstate.lock.info" \
+    -o -name "terraform.tfstate.backup" \
+    -o -name "terraform.tfstate" \
+    -o -name "myplan" \
+\) -exec rm -f {} +
