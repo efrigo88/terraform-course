fcccf609ec57da8098d0c1a27f1c878d19be5c59
Emiliano Frigo <emif.iron@gmail.com>
2025-01-02 11:03:13 +0100

    add excercise 7

---
+++ 07-data-sources/compute.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Owner ID is Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+output "ubuntu_ami_data" {
+  value = data.aws_ami.ubuntu.id
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+}
