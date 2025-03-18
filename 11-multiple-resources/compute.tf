f5f64ec56bedccd57c7e2315ca01d6a58167efdc
Emiliano Frigo <emif.iron@gmail.com>
2025-03-18 20:17:23 +0100

    add to ex 11

--- 11-multiple-resources/compute.tf
+++ 11-multiple-resources/compute.tf
@@ -1,3 +1,9 @@
+locals {
+  ami_ids = {
+    ubuntu = data.aws_ami.ubuntu.id
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Owner ID is Canonical
@@ -17,6 +23,18 @@ resource "aws_instance" "from_count" {
   count         = var.ec2_instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
+
+  tags = {
+    Name    = "${local.project}-${count.index}"
+    Project = local.project
+  }
+}
+
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
   subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
 
   tags = {
