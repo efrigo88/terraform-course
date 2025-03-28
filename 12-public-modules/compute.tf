dccee31164d923767b26b92dba6cec28225e8ab8
Emiliano Frigo <emif.iron@gmail.com>
2025-03-28 11:02:09 +0100

    add fmt

--- 12-public-modules/compute.tf
+++ 12-public-modules/compute.tf
@@ -1,5 +1,5 @@
 locals {
-    instance_type = "t2.micro"
+  instance_type = "t2.micro"
 }
 
 data "aws_ami" "ubuntu" {
@@ -19,14 +19,14 @@ data "aws_ami" "ubuntu" {
 }
 
 module "ec2" {
-    source = "terraform-aws-modules/ec2-instance/aws"
-    version = "5.7.1"
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.1"
 
-    name = local.project_name
-    ami = data.aws_ami.ubuntu.id
-    instance_type = local.instance_type
-    vpc_security_group_ids = [module.vpc.default_security_group_id]
-    subnet_id = module.vpc.public_subnets[0]
+  name                   = local.project_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = local.instance_type
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
-    tags = local.common_tags
+  tags = local.common_tags
 }
