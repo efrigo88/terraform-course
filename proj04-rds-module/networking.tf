a41cdf057398330c3c942836b3b9218b9959c57a
Emiliano Frigo <emif.iron@gmail.com>
2025-03-31 11:55:29 +0200

    proj4-rds-module

--- proj04-rds-module/networking.tf
+++ proj04-rds-module/networking.tf
@@ -1,3 +1,6 @@
+########################################################
+# VPC and subnets
+########################################################
 data "aws_vpc" "default" {
   default = true
 }
@@ -48,3 +51,47 @@ resource "aws_subnet" "public" {
 #     Name = "subnet-default-vpc"
 #   }
 # }
+
+########################################################
+# Security groups
+########################################################
+
+# 1. Source security group - From where traffic is allowed
+# 2. Compliant security group
+#   2.1 Security group rule
+# 3. Non-compliant security group
+#   3.1 Security group rule
+
+resource "aws_security_group" "source" {
+  name        = "source-sg"
+  description = "SG from where connections are allowed into the DB"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_security_group" "compliant" {
+  name        = "compliant-sg"
+  description = "Compliant SG"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db" {
+  security_group_id            = aws_security_group.compliant.id
+  referenced_security_group_id = aws_security_group.source.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_security_group" "non_compliant" {
+  name        = "non-compliant-sg"
+  description = "Non-compliant SG"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.non_compliant.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
